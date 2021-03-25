5d207603bcbfebcb31e7e0a20e25584fe834a671
3epr <tpynope31@gmail.com>
2021-03-25 16:28:42 +0300

    host

---
+++ host.py
@@ -0,0 +1,26 @@
+import socket, time, pynput
+
+host = socket.gethostbyname(socket.gethostname())
+port = 9090
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+
+q = False
+print('[ Server Started ]')
+
+while not q:
+    try:
+        data, addr = s.recvfrom(1024)
+
+        if addr not in clients:
+            clients.append(addr)
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
+    except:
+        print('[ Server Stopped ]')
+        q = True
+s.close()
